1e2ef0cfa24e60171c1934c6a9a08bdfe93b722f
AggroTheShark <aresgf03@gmail.com>
2025-02-27 20:13:37 +0100

    Refactorización GameService Añadidos métodos de utilidad para obtener preguntas con aleatoriedad controlada

--- gameservice/questions-service.js
+++ gameservice/questions-service.js
@@ -40,27 +40,77 @@ async function sendQuery(template) {
     }
 }
 
-async function generateQuestion(results, template){
-    const paisCorrecto = results[Math.floor(Math.random() * results.length)]
-    const respuestasIncorrectas = []
-    while(respuestasIncorrectas.length < NUMBER_OF_WRONG_ANSWERS){
-        const ranNumber = Math.floor(Math.random() * results.length)
-        if(results[ranNumber].country != paisCorrecto.country && !respuestasIncorrectas.includes(results[ranNumber])){
-            respuestasIncorrectas.push(results[ranNumber].country)
-        }
+/**
+ * Aux. function to generate random IDs.
+ * @param {int} length Length of the original array 
+ * @param {int} count Number of random IDs to generate
+ * @returns {Array} Array containing the random IDs
+ */
+function randomIDs(length, count)
+{
+    let arr = Array.from({length: length}, (v, k) => k);
+
+    for ( let i = arr.length - 1; i > 0; i-- )
+    {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
     }
-    const title = template.question.replace('*', paisCorrecto.flag)
-    const answers = paisCorrecto.country + "," + respuestasIncorrectas[0] + "," + respuestasIncorrectas[1] + "," + respuestasIncorrectas[2]
-    const newQuestion = Question({
-        title: title,
-        correctAnswer: paisCorrecto.country,
-        allAnswers: answers
-    })
-    await newQuestion.save()
-    return newQuestion
+
+    return arr.slice(0,count);
 }
 
-//Prueba para probar si el servicio está activo
+/**
+ * Aux. function to shuffle an array.
+ * @param {Array} array Array to shuffle 
+ * @returns {Array} Shuffled array
+ */
+function shuffleArray(array)
+{
+    for ( let i = array.length - 1; i > 0; i-- )
+    {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+/**
+ * Given a full array of results and a question template, generates a question.
+ * 
+ * The returned question contains a title with the flag of the correct country
+ * and a list of 4 shuffled answers - 1 correct and 3 incorrect.
+ * @param {Array} results Array of Wikidata query results
+ * @param {JSON} template Question template
+ * @returns {JSON} Question data object
+ */
+async function generateQuestion(results, template)
+{
+    // Get 4 random answers - 1 correct and 3 incorrect
+    const randomIDs = randomIDs(results.length, NUMBER_OF_WRONG_ANSWERS + 1);
+        const correctID = randomIDs[0];           // Correct answer ID
+        const incorrectIDs = randomIDs.slice(1);  // Incorrect answers IDs
+
+    // Answers
+    const correctAnswer = results[correctID];
+    const incorrectAnswers = incorrectIDs.map(id => results[id]);
+
+    // Shuffle answers
+    const answers = shuffleArray([correctAnswer, ...incorrectAnswers]);
+
+    // Compound returned JSON object
+    const title = template.question.replace('*', correctAnswer.flag);
+    const newQuestion = Question(
+    {
+        title: title,
+        correctAnswer: correctAnswer.country,
+        allAnswers: answers
+    });
+
+    await newQuestion.save();
+    return newQuestion;
+}
+
+// Test if service is active
 app.get('/test', (req, res) => {
     res.json({ status: 'OK' });
 })
@@ -78,8 +128,6 @@ app.get('/add-test', async (req, res) =>{
     const newQuestion = await generateQuestion(results, template)
     res.json(newQuestion)
 });
-
-
 
 const server = app.listen(port, () => {
     console.log(`Question Service listening at http://localhost:${port}`);
