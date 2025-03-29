541d7e2e3352588e1c29bca086ae54acc120155b
David Álvarez Cabezas <davidalvarezcabezas@gmail.com>
2025-03-29 11:53:11 +0100

    Arreglado el test base de Login

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
-const { defineFeature, loadFeature }=require('jest-cucumber');
-const setDefaultOptions = require('expect-puppeteer').setDefaultOptions
+const { defineFeature, loadFeature } = require('jest-cucumber');
+const setDefaultOptions = require('expect-puppeteer').setDefaultOptions;
 const feature = loadFeature('./features/register-form.feature');
 
 let page;
@@ -10,43 +10,41 @@ defineFeature(feature, test => {
   
   beforeAll(async () => {
     browser = process.env.GITHUB_ACTIONS
-      ? await puppeteer.launch({headless: "new", args: ['--no-sandbox', '--disable-setuid-sandbox']})
+      ? await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] })
       : await puppeteer.launch({ headless: false, slowMo: 100 });
     page = await browser.newPage();
-    //Way of setting up the timeout
-    setDefaultOptions({ timeout: 10000 })
+    
+    setDefaultOptions({ timeout: 10000 });
 
-    await page
-      .goto("http://localhost:3000", {
-        waitUntil: "networkidle0",
-      })
-      .catch(() => {});
+    await page.goto('http://localhost:3000', { waitUntil: 'networkidle0' }).catch(() => {});
   });
 
-  test('The user is not registered in the site', ({given,when,then}) => {
-    
-    let username;
-    let password;
+  test('The user is not registered in the site', ({ given, when, then }) => {
+    let email, password;
 
     given('An unregistered user', async () => {
-      username = "pablo"
-      password = "pabloasw"
-      await expect(page).toClick("button", { text: "Don't have an account? Register here." });
+      email = 'unregistered@example.com';
+      password = 'wrongpassword';
     });
 
-    when('I fill the data in the form and press submit', async () => {
-      await expect(page).toFill('input[name="username"]', username);
-      await expect(page).toFill('input[name="password"]', password);
-      await expect(page).toClick('button', { text: 'Add User' })
+    when('I fill the data in the login form and press submit', async () => {
+      await page.waitForSelector('input[type="email"]', { visible: true });
+      await page.type('input[type="email"]', email);
+      
+      await page.waitForSelector('input[type="password"]', { visible: true });
+      await page.type('input[type="password"]', password);
+
+      await page.waitForSelector('button.login-button', { visible: true });
+      await page.click('button.login-button');
     });
 
-    then('A confirmation message should be shown in the screen', async () => {
-        await expect(page).toMatchElement("div", { text: "User added successfully" });
+    then('An error message should be shown in the screen', async () => {
+      await page.waitForSelector('.MuiSnackbar-root', { visible: true });
+      await expect(page).toMatchElement('.MuiSnackbar-root', { text: 'Error al iniciar sesión' });
     });
-  })
+  });
 
-  afterAll(async ()=>{
-    browser.close()
-  })
-
+  afterAll(async () => {
+    await browser.close();
+  });
 });
