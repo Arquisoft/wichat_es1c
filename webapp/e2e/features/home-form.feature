6a5660ca31b3a92a98dc1f36f266fce440875f1d
David Álvarez Cabezas <davidalvarezcabezas@gmail.com>
2025-03-31 00:54:35 +0200

    Test básico de home

---
+++ webapp/e2e/features/home-form.feature
@@ -0,0 +1,6 @@
+Feature: Home page functionality
+
+  Scenario: User sees a personalized welcome message
+    Given The user logins
+    When The user is redirected to the Home page
+    Then The home page should display the message "Nos alegra verte de nuevo. Explora tus rankings y disfruta de la experiencia de WiChat."
---
+++ webapp/e2e/steps/home-form.steps.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+const { defineFeature, loadFeature } = require('jest-cucumber');
+const setDefaultOptions = require('expect-puppeteer').setDefaultOptions;
+const feature = loadFeature('./features/home-form.feature');
+
+let page;
+let browser;
+let email = `test@test2`;
+let password = 'test';
+
+defineFeature(feature, test => {
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ headless: false, slowMo: 0 });
+    page = await browser.newPage();
+    setDefaultOptions({ timeout: 10000 });
+
+    await page.goto('http://localhost:3000', { waitUntil: 'networkidle0' });
+  });
+
+  test('User sees a personalized welcome message', ({ given, when, then }) => {
+    given('The user logins', async () => {
+        await page.goto('http://localhost:3000'); 
+        await page.waitForSelector('.login-container');
+        await page.type('input[type="email"]', email);
+        await page.type('input[type="password"]', password);
+        await page.click('button.login-button');
+        await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    });
+
+    when('The user is redirected to the Home page', async () => {
+        const url = await page.url();
+        expect(url).toBe('http://localhost:3000/home');
+    });
+
+    then('The home page should display the message "Nos alegra verte de nuevo. Explora tus rankings y disfruta de la experiencia de WiChat."', async () => {
+        const welcomeMessage = await page.$eval('p', el => el.textContent);
+        expect(welcomeMessage).toContain('Nos alegra verte de nuevo. Explora tus rankings y disfruta de la experiencia de WiChat.');
+      });
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+});
