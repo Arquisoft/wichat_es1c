b3e3487fa8b4373b17e0ff593d58b33f81f3959d
David Álvarez Cabezas <davidalvarezcabezas@gmail.com>
2025-03-31 00:59:28 +0200

    Tests de home terminados

--- webapp/e2e/features/home-form.feature
+++ webapp/e2e/features/home-form.feature
@@ -1,6 +1,11 @@
 Feature: Home page functionality
 
-  Scenario: User sees a personalized welcome message
+  Scenario: User sees a welcome message
     Given The user logins
     When The user is redirected to the Home page
     Then The home page should display the message "Nos alegra verte de nuevo. Explora tus rankings y disfruta de la experiencia de WiChat."
+
+  Scenario: User sees a personalized welcome message
+    Given The user logins
+    When The user is redirected to the Home page
+    Then The home page should display a personalized welcome message including the user's name
--- webapp/e2e/steps/home-form.steps.js
+++ webapp/e2e/steps/home-form.steps.js
@@ -7,6 +7,7 @@ let page;
 let browser;
 let email = `test@test2`;
 let password = 'test';
+let userName = 'test';
 
 defineFeature(feature, test => {
   beforeAll(async () => {
@@ -17,7 +18,7 @@ defineFeature(feature, test => {
     await page.goto('http://localhost:3000', { waitUntil: 'networkidle0' });
   });
 
-  test('User sees a personalized welcome message', ({ given, when, then }) => {
+  test('User sees a welcome message', ({ given, when, then }) => {
     given('The user logins', async () => {
         await page.goto('http://localhost:3000'); 
         await page.waitForSelector('.login-container');
@@ -36,6 +37,28 @@ defineFeature(feature, test => {
         const welcomeMessage = await page.$eval('p', el => el.textContent);
         expect(welcomeMessage).toContain('Nos alegra verte de nuevo. Explora tus rankings y disfruta de la experiencia de WiChat.');
       });
+  });
+
+
+  test('User sees a personalized welcome message', ({ given, when, then }) => {
+    given('The user logins', async () => {
+      await page.goto('http://localhost:3000');
+      await page.waitForSelector('.login-container');
+      await page.type('input[type="email"]', email);
+      await page.type('input[type="password"]', password);
+      await page.click('button.login-button');
+      await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    });
+
+    when('The user is redirected to the Home page', async () => {
+      const url = await page.url();
+      expect(url).toBe('http://localhost:3000/home');
+    });
+
+    then('The home page should display a personalized welcome message including the user\'s name', async () => {
+      const welcomeMessage = await page.$eval('h2', el => el.textContent);  
+      expect(welcomeMessage).toContain(`¡Bienvenido de nuevo, ${userName}!`);
+    });
   });
 
   afterAll(async () => {
