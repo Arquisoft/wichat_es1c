794d41172899ba6b5853efc7da9eb629edc8724b
Mario Garcia <uo279079@uniovi.es>
2025-03-31 14:53:09 +0200

    Eliminacion de libreria

--- webapp/src/components/UserAccount.test.js
+++ webapp/src/components/UserAccount.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import axios from 'axios';
 import UserAccount from './UserAccount';
 import { MemoryRouter } from 'react-router-dom';
@@ -58,12 +57,10 @@ describe('UserAccount Component', () => {
       </MemoryRouter>
     );
 
-    await waitFor(() => {
-      expect(screen.getByText('Total de partidas jugadas: 2')).toBeInTheDocument();
-      expect(screen.getByText('Preguntas acertadas: 14')).toBeInTheDocument();
-      expect(screen.getByText('Preguntas falladas: 6')).toBeInTheDocument();
-      expect(screen.getByText('Total de preguntas: 20')).toBeInTheDocument();
-    });
+    expect(await screen.findByText('Total de partidas jugadas: 2')).toBeInTheDocument();
+    expect(await screen.findByText('Preguntas acertadas: 14')).toBeInTheDocument();
+    expect(await screen.findByText('Preguntas falladas: 6')).toBeInTheDocument();
+    expect(await screen.findByText('Total de preguntas: 20')).toBeInTheDocument();
   });
 
   test('handles API error gracefully', async () => {
@@ -75,10 +72,9 @@ describe('UserAccount Component', () => {
       </MemoryRouter>
     );
 
-    await waitFor(() => {
-      expect(screen.getByText('Usuario: Test User')).toBeInTheDocument();
-      expect(screen.getByText('Correo: test@example.com')).toBeInTheDocument();
-    });
+    expect(await screen.findByText('Usuario: Test User')).toBeInTheDocument();
+    expect(await screen.findByText('Correo: test@example.com')).toBeInTheDocument();
+
   });
   test('decodes token and sets user name and email', async () => {
     render(
@@ -87,10 +83,8 @@ describe('UserAccount Component', () => {
         </MemoryRouter>
     );
 
-    await waitFor(() => {
-        expect(screen.getByText('Usuario: Test User')).toBeInTheDocument();
-        expect(screen.getByText('Correo: test@example.com')).toBeInTheDocument();
-    });
+    expect(await screen.findByText('Usuario: Test User')).toBeInTheDocument();
+    expect(await screen.findByText('Correo: test@example.com')).toBeInTheDocument();
 });
 
 test('handles invalid token gracefully', async () => {
@@ -105,10 +99,8 @@ test('handles invalid token gracefully', async () => {
         </MemoryRouter>
     );
 
-    await waitFor(() => {
-        expect(screen.queryByText('Usuario:')).not.toBeInTheDocument();
-        expect(screen.queryByText('Correo:')).not.toBeInTheDocument();
-    });
+    expect(await screen.findByText('Usuario:')).not.toBeInTheDocument();
+    expect(await screen.findByText('Correo:')).not.toBeInTheDocument();
 });
 
 test('renders the chart when last 10 games exist', async () => {
