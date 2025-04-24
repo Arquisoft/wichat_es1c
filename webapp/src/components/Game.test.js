23614fbe2f135f041d651963cefdb824786ef528
David Álvarez Cabezas <davidalvarezcabezas@gmail.com>
2025-04-24 18:51:45 +0200

    Añadidos tests de game

--- webapp/src/components/Game.test.js
+++ webapp/src/components/Game.test.js
@@ -83,4 +83,34 @@ describe('Game component', () => {
             expect(screen.getByText('Incorrecto.')).toBeInTheDocument();
         });
     });
+
+    it('cambia a la vista del juego cuando se inicia', async () => {
+        render(<BrowserRouter><Game /></BrowserRouter>);
+        fireEvent.click(screen.getByTestId('start-game'));
+    
+        await waitFor(() => {
+            expect(screen.getByText('¿Capital de Francia?')).toBeInTheDocument();
+        });
+    });
+
+    it('reproduce sonido correcto al seleccionar respuesta correcta', async () => {
+        const audioMock = jest.spyOn(window.HTMLAudioElement.prototype, 'play').mockImplementation(() => {});
+    
+        render(<BrowserRouter><Game /></BrowserRouter>);
+        fireEvent.click(screen.getByTestId('start-game'));
+    
+        await waitFor(() => screen.getByText('¿Capital de Francia?'));
+    
+        const correctBtn = screen.getByText('París');
+    
+        fireEvent.click(correctBtn);
+    
+        await waitFor(() => {
+            expect(audioMock).toHaveBeenCalled();
+            expect(audioMock).toHaveBeenCalledWith();
+        });
+    
+        audioMock.mockRestore(); 
+    });
+    
 });
