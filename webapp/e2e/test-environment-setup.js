ae8653e5481f16c6b8df260e16c5d5b747e8884b
Dani <uo283794@uniovi.es>
2025-04-27 20:15:15 +0200

    Ejecucion test E2E (fix)

--- webapp/e2e/test-environment-setup.js
+++ webapp/e2e/test-environment-setup.js
@@ -5,18 +5,21 @@ let userservice;
 let authservice;
 let llmservice;
 let gatewayservice;
-let gameservice
+let gameservice;
 
 async function startServer() {
     console.log('Starting MongoDB memory server...');
     mongoserver = await MongoMemoryServer.create();
     const mongoUri = mongoserver.getUri();
     process.env.MONGODB_URI = mongoUri;
+
     userservice = await require("../../userservice/userservice");
     authservice = await require("../../authservice/auth");
     llmservice = await require("../../llmservice/chatbot");
     gatewayservice = await require("../../gatewayservice/gateway-service");
     gameservice = await require("../../gameservice/questions-service");
+    
+
 }
 
 startServer();
