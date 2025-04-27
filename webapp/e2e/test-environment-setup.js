ad82beb8597fa9f1bceab8b445ab21cd0d2c91db
Dani <uo283794@uniovi.es>
2025-04-27 22:43:33 +0200

    E2E con Mongo (En Memoria)

--- webapp/e2e/test-environment-setup.js
+++ webapp/e2e/test-environment-setup.js
@@ -1,4 +1,5 @@
 const { MongoMemoryServer } = require('mongodb-memory-server');
+const { addUserToDatabase } = require('./addTestUser');
 
 let mongoserver;
 let userservice;
@@ -12,14 +13,20 @@ async function startServer() {
     mongoserver = await MongoMemoryServer.create();
     const mongoUri = mongoserver.getUri();
     process.env.MONGODB_URI = mongoUri;
+    process.env.REACT_APP_API_ENDPOINT = "http://localhost:8000";
 
     userservice = await require("../../userservice/userservice");
     authservice = await require("../../authservice/auth");
     llmservice = await require("../../llmservice/chatbot");
     gatewayservice = await require("../../gatewayservice/gateway-service");
     gameservice = await require("../../gameservice/questions-service");
-    
 
+    console.log('Esperando a que los servicios arranquen...');
+    await new Promise(resolve => setTimeout(resolve, 3000)); // pequeño delay para asegurar que levantan
+
+    console.log('Añadiendo usuario test@test...');
+    await addUserToDatabase('test@test', 'test');
+    console.log('Usuario añadido.');
 }
 
 startServer();
